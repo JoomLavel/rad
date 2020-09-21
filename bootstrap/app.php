5971bf1f304f23fb25743bb6396903d33c6e7f5f
jwalczak <32653379+q-u-o-s-a@users.noreply.github.com>
2020-09-21 20:48:08 +0200

    Composer PSR-4 fixes

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -6,10 +6,10 @@
  * Time: 18:42
  */
 
-require __DIR__ . '/../scr/command/GenericCommand.php';
-require __DIR__ . '/../scr/command/MakeComponent.php';
-require __DIR__ . '/../scr/service/ComponentGenerator.php';
-require __DIR__ . '/../scr/service/ZipGenerator.php';
+require __DIR__ . '/../src/Command/GenericCommand.php';
+require __DIR__ . '/../src/Command/MakeComponent.php';
+require __DIR__ . '/../src/Service/ComponentGenerator.php';
+require __DIR__ . '/../src/Service/ZipGenerator.php';
 
 use JoomLavel\Rad\Command\MakeComponent;
 use Symfony\Component\Console\Application;
